d7f49575e4c47f15355917701fab7cec32626857
WilliamBaldridge <baldridge.william09@gmail.com>
2021-02-26 17:45:13 -0600

    working on loading screen

--- js/home.js
+++ js/home.js
@@ -2,21 +2,57 @@
 {
     const url = "https://thoughtful-equatorial-beech.glitch.me/movies"
 
-//Function fetch all data from json
-    const isLoading = true;
-    function onLoad(isLoading) {
+// Function fetch all data from json
+    function onLoad() {
         return new Promise(((resolve, reject) => {
-            if (Promise === "pending"){
-                resolve("Loading")
-            } else {
-                reject("Error")
-            }
+            resolve(fetch(url)
+                .then(response => response.json())
+                .then(data => console.log(data)));
+                // .then($("#loading").hide())
+
+            reject("Error")
         }))
     }
+
     onLoad().then(console.log);
-    console.log(onLoad(true));
+    //
+    // console.log(onLoad(false));
+
+
+    const getAllMovies = () => fetch(url)
+    .then(response => response.json())
+        .then(data => {
+            data.map(function (i) {
+              return i
+            })
+            const [{title, rating}] = data;
+            document.write(`<h1>${title}: ${rating}</h1>`);
+        })
+    .catch(console.error);
+
+    console.log(getAllMovies());
+    // getAllMovies()
+    //     .then();
+    getAllMovies();
+
+
+// const getMovie = id => fetch(`${url}/${id}`)
+//     .then(response => response.json())
+//     .catch(console.error);
+//
+// getMovie(2).then(movie => {
+//     const [{rating, title}] = movie
+//     document.write(`<h1>${title}: ${rating}</h1>`);
+// })
+
+
+
+
+
+
 
 
 
 
 }
+
