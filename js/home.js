15aac82682e186342e8263e0279b7164553ca569
WilliamBaldridge <baldridge.william09@gmail.com>
2021-02-27 15:18:04 -0600

    adds CRUD functions, starting HTML, and OMDb API info

--- js/home.js
+++ js/home.js
@@ -1,14 +1,19 @@
 "use strict";
 {
-    const url = "https://thoughtful-equatorial-beech.glitch.me/movies"
+    const URL = "https://thoughtful-equatorial-beech.glitch.me/movies"
+    const OMDb_URL = "http://www.omdbapi.com/"
+    const OMDb_KEY = OMDb_API
+    const OMDb_Title_Search = `?t=${$("#movie-search").val()}&${OMDb_KEY}`
+    // add some logic to determine "search criteria" and use correct endpoint (i.e ?t=, ?y=, ?type=)
+
 
 // Function fetch all data from json
     function onLoad() {
         return new Promise(((resolve, reject) => {
-            resolve(fetch(url)
+            resolve(fetch(URL)
                 .then(response => response.json())
                 .then(data => console.log(data)));
-                // .then($("#loading").hide())
+            // .then($("#loading").hide())
 
             reject("Error")
         }))
@@ -19,33 +24,78 @@
     // console.log(onLoad(false));
 
 
-    const getAllMovies = () => fetch(url)
-    .then(response => response.json())
+    const getAllMovies = () => fetch(URL)
+        .then(response => response.json())
         .then(data => {
             data.map(function (i) {
-              return i
+                return i
             })
             const [{title, rating}] = data;
             document.write(`<h1>${title}: ${rating}</h1>`);
         })
-    .catch(console.error);
+        .catch(console.error);
 
     console.log(getAllMovies());
     // getAllMovies()
     //     .then();
     getAllMovies();
 
+// Retrieve movie by id (i= with OMDb)
+    const getMovieById = id => fetch(`${URL}/${id}`)
+        .then(response => response.json())
+        .catch(console.error);
 
-// const getMovie = id => fetch(`${url}/${id}`)
-//     .then(response => response.json())
-//     .catch(console.error);
-//
-// getMovie(2).then(movie => {
-//     const [{rating, title}] = movie
-//     document.write(`<h1>${title}: ${rating}</h1>`);
-// })
+    getMovieById(2).then(movie => {
+        const [{rating, title}] = movie
+        document.write(`<h1>${title}: ${rating}</h1>`);
+    });
 
+    // Retrieve movie by title (t= with OMDb)
+    const getMovieByTitle = id => fetch(`${URL}/${id}`)
+        .then(response => response.json())
+        .catch(console.error);
 
+    getMovieByTitle(2).then(movie => {
+        const [{rating, title}] = movie
+        document.write(`<h1>${title}: ${rating}</h1>`);
+    });
+
+    // Retrieve movie by rating
+    const getMovieByRating = id => fetch(`${URL}/${id}`)
+        .then(response => response.json())
+        .catch(console.error);
+
+    getMovieByRating(2).then(movie => {
+        const [{rating, title}] = movie
+        document.write(`<h1>${title}: ${rating}</h1>`);
+    });
+
+    const editMovie = movie => fetch(`${URL}/${movie.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(movie)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(`Movie edit successful ${JSON.stringify(data)}`)
+        })
+        .catch(console.error);
+
+    const addMovie = movie => fetch(`${URL}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(movie)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(`Movie addition success ${JSON.stringify(movie)}`);
+            return movie.id;
+        })
+        .catch(console.error);
 
 
 
